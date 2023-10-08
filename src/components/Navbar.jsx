d7a57152768181efedb28cd5bf10479978f6cbca
yoelwildanto <yoel.wildantop@gmail.com>
2023-10-08 12:27:39 +0700

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,42 @@ import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import { PRODUCTS } from "../products2";
 import { ShopContext } from "../context/shop-context";
+import { NavCart } from "./navbarCart";
 import "./navbar.css";
 
 export const Navbar = () => {
-  const { cartItems} = useContext(ShopContext)
+  const { cartItems, updateCartItemCount, getTotalCartAmount} = useContext(ShopContext)
   const total =Object.values(cartItems)
   const sum=total.reduce((sum, item) => sum + item,0)
-  console.log(sum)
+  const totalAmount = getTotalCartAmount();
+
+  // console.log(updateCartItemCount)
 
   return (
     <div className="navbar">
       <div className="links">
         <Link to="/"> Shop </Link>
-        <Link to="/contact"> Contact </Link>
-        <Link to="/cart">
+        <Link to="/contact" className="contact"> Contact </Link>
+        <div to="/cart" className="total-items">
           <ShoppingCart size={32} />
-        </Link>
-        <div className="total-items">{sum}</div>
+          {sum}
+          <div className="dropdown-content"> 
+          <div className="cartdrop">
+          {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <NavCart data={product} />;
+          }
+           })} 
+           <h3 className="total"> Total: ${totalAmount}</h3>
+           <Link to={"/cart"} className="submit">
+           <div className="bayar">        
+           Bayar
+           </div> 
+           </Link>
+        </div>
+        </div>
+        </div>
+        {/* <div className="total-items">{sum}</div> */}
       </div>
     </div>
   );
---
+++ src/components/navbarCart.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../context/shop-context";
+import {BsTrash} from "react-icons/bs"
+// import {Link} from "react-router-dom"
+
+export const NavCart = (props) =>{
+    const { id, productName, price, image } = props.data;
+    const { cartItems, addToCart, removeFromCart, } =
+      useContext(ShopContext);
+
+      const cartItemCount = cartItems[id];
+
+
+    return (
+        <div className="cartItem-drop">
+        <img src={image} />
+        <div className="description-drop">
+            <div className="cartItem-name">
+          <p>
+            <b>{productName}</b>
+          </p>
+          </div>
+          <div className="cartItem-harga">
+          <p> Price: ${price}</p>
+          <p>Qty: {cartItemCount}</p>
+          </div>
+          {/* <Link to={"/cart"} className="submit"> Bayar </Link> */}
+          </div>
+          <button className="trash" onClick={() => removeFromCart(id)}><BsTrash/></button>
+        </div>
+    )
+}
